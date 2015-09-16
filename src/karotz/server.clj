f23de1cd3a3fb4ce0c2bcae1efc6541a88b8d800
Zilvinas Kybartas <zilvinas@inventi.lt>
2015-09-16 16:51:09 +0300

    add sample thinking function

--- src/karotz/server.clj
+++ src/karotz/server.clj
@@ -5,12 +5,24 @@
    ring.middleware.keyword-params
    [org.httpkit.server :only [run-server]])
   (:require [compojure.route :as route]
-            [compojure.handler :as handler]))
+            [compojure.handler :as handler]
+            [karotz.api :as api]))
 
 (defn wrap-dir-index [handler]
   (fn [req]
     (handler
       (update-in req [:uri] #(if (= "/" %) "/index.html" %)))))
+
+(defn thinking [ip]
+  (api/leds ip "ff0000" 700)
+  (api/ears ip 1 1)
+  (Thread/sleep 2000)
+  (api/leds ip "fff200" 500)
+  (Thread/sleep 2000)
+  (api/leds ip "00b7ff" 300)
+  (Thread/sleep 2000)
+  (api/leds ip "00ff09" 0)
+  (api/say ip "Greetings Antanas. Relax, it's going to be OK. Unless you're a software architect, in which case there's no hope for you."))
 
 (def api-routes
   (->
